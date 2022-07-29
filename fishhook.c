164f095e1fec7d0408db3a5f33386692c43d9bc3
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2022-07-29 13:16:06 +0700

    Comment out #if !__has_feature(ptrauth_calls)

--- fishhook.c
+++ fishhook.c
@@ -157,16 +157,16 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
              * iOS 15 has corrected the const segments prot.
              * -- Lionfore Hao Jun 11th, 2021
              **/
-            #if !__has_feature(ptrauth_calls)
+            //#if !__has_feature(ptrauth_calls)
             indirect_symbol_bindings[i] = cur->rebindings[j].replacement;
-            #else
+            //#else
             void *replacement = cur->rebindings[j].replacement;
             if (!strcmp(section->sectname, "__auth_got")) {
               void *stripped = ptrauth_strip(replacement, ptrauth_key_process_independent_code);
               replacement = ptrauth_sign_unauthenticated(stripped, ptrauth_key_process_independent_code, &indirect_symbol_bindings[i]);
             }
             indirect_symbol_bindings[i] = replacement;
-            #endif
+            //#endif
           }
           goto symbol_loop;
         }
